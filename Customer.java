75ba4cf3c95cac9243ea68fe1920fe192b12a4a3
Felipe Kenji <kenjissauro9@gmail.com>
2025-11-27 16:45:50 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -34,6 +34,27 @@ public class Customer {
                  " frequent renter points";
         return result;
     }
+    
+    // NOVO MÉTODO: htmlStatement()
+    public String htmlStatement() {
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+        
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+
+            // show figures for each rental
+            result += each.getMovie().getTitle() + ": " +
+                String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        
+        // add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM></P>\n";
+        result += "<P>On this rental you earned <EM>" +
+            String.valueOf(getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points</P>";
+        return result;
+    }
 
     private double getTotalCharge() {
         double result = 0;
