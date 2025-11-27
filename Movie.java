d48871f3d8c72ba9322500524519a6c5dd50e5b2
Felipe Kenji <kenjissauro9@gmail.com>
2025-11-27 16:55:19 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -1,5 +1,5 @@
 public class Movie {
-    // ... (atributos, construtor, get/setPriceCode, getTitle, getCharge inalterados) ...
+
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
@@ -11,13 +11,35 @@ public class Movie {
         _title = title;
         setPriceCode(priceCode);
     }
-    // ... (resto da classe Movie) ...
+
+    public int getPriceCode() {
+        return _price.getPriceCode();
+    }
+
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
+
+    public String getTitle (){
+        return _title;
+    }
 
     public double getCharge(int daysRented) {
         return _price.getCharge(daysRented);
     }
 
-    // MÉTODO MODIFICADO: Delega a chamada para o objeto _price
     public int getFrequentRenterPoints(int daysRented) {
         return _price.getFrequentRenterPoints(daysRented);
     }
--- Price.java
+++ Price.java
@@ -1,6 +1,5 @@
 public abstract class Price {
     public abstract int getPriceCode();
     public abstract double getCharge(int daysRented);
-    // NOVO MÉTODO ABSTRATO
     public abstract int getFrequentRenterPoints(int daysRented); 
 }
